cccc149d9d3368ccfe5d1ea49a1e1e3b923d8072
Richard Blais <richard@neovation.com>
2016-10-21 07:56:33 -0500

    Style todo application

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -58,9 +58,17 @@ var TodoApp = React.createClass({
 
     return (
       <div>
-        <TodoSearch onSearch={this.handleSearch} />
-        <TodoList onToggle={this.handleToggle} todos={filteredTodos} />
-        <AddTodo onAddTodo={this.handleAddTodo} />
+        <h1 className="page-title">Todo App</h1>
+
+        <div className="row">
+          <div className="column small-centered small-11 medium-6 large-5">
+            <div className="container">
+              <TodoSearch onSearch={this.handleSearch} />
+              <TodoList onToggle={this.handleToggle} todos={filteredTodos} />
+              <AddTodo onAddTodo={this.handleAddTodo} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -20,10 +20,17 @@ describe('TodoList', () => {
       id: 2,
       text: 'Check mail'
     }];
-
     var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
     var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-    
+
     expect(todosComponents.length).toBe(todos.length);
+  });
+
+  it('should render empty message if no todos', () => {
+    var todos = [];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    expect($el.find('.container__message').length).toBe(1);
   });
 });
